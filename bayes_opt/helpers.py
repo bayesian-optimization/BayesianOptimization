bfac53eb8b169399186226022b96da1653ad165c
stm <stmax82@gmail.com>
2016-03-07 09:44:59 +0100

    adjusted POI and UCB acquisition functions to use batch-wise predictions

--- bayes_opt/helpers.py
+++ bayes_opt/helpers.py
@@ -34,22 +34,12 @@ class UtilityFunction(object):
 
     @staticmethod
     def _ucb(x, gp, kappa):
-        mean, var = gp.predict(x, eval_MSE=True)
+        mean, var = self._gp_predict(gp, x)
         return mean + kappa * np.sqrt(var)
 
     @staticmethod
     def _ei(x, gp, y_max):
-        #mean, var = gp.predict(x, eval_MSE=True)
-
-        cuts = list(range(0, len(x), 1000)) + [len(x)]
-
-        mean, var = [], []
-        for a, b in zip(cuts, cuts[1:]):
-            m, v = gp.predict(x[a:b], eval_MSE=True)
-            mean += m
-            var  += v
-        mean = np.concatenate(mean)
-        var  = np.concatenate(var)
+        mean, var = self._gp_predict(gp, x)
 
         # Avoid points with zero variance
         var = np.maximum(var, 1e-9 + 0 * var)
@@ -59,13 +49,30 @@ class UtilityFunction(object):
 
     @staticmethod
     def _poi(x, gp, y_max):
-        mean, var = gp.predict(x, eval_MSE=True)
+        mean, var = self._gp_predict(gp, x)
 
         # Avoid points with zero variance
         var = np.maximum(var, 1e-9 + 0 * var)
 
         z = (mean - y_max)/np.sqrt(var)
         return norm.cdf(z)
+
+    @staticmethod
+    def _gp_predict(gp, x):
+        # gp.predict takes a lot of memory, so we feed batches to it.
+        # gp.predict(x, batch_size=1000) would be preferable, but that
+        # doesn't work with python 3 as of sklearn 0.17.1.
+        cuts = list(range(0, len(x), 1000)) + [len(x)]
+
+        mean, var = [], []
+        for a, b in zip(cuts, cuts[1:]):
+            m, v = gp.predict(x[a:b], eval_MSE=True)
+            mean += m
+            var  += v
+        mean = np.concatenate(mean)
+        var  = np.concatenate(var)
+        
+        return mean, var
 
 
 def unique_rows(a):
