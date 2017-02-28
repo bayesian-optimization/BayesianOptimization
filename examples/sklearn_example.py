1ce5484c6fff6e4913d43fa41dbed29f2a95f187
fmfn <fmfnogueira@gmail.com>
2017-02-28 00:37:19 -0500

    fixes alpha value in sklearn example and runs notebook till the end

--- examples/sklearn_example.py
+++ examples/sklearn_example.py
@@ -9,37 +9,51 @@ from sklearn.svm import SVC
 from bayes_opt import BayesianOptimization
 
 # Load data set and target values
-data, target = make_classification(n_samples=1000,
-                                   n_features=45,
-                                   n_informative=12,
-                                   n_redundant=7)
+data, target = make_classification(
+    n_samples=1000,
+    n_features=45,
+    n_informative=12,
+    n_redundant=7
+)
 
 def svccv(C, gamma):
-    return cross_val_score(SVC(C=C, gamma=gamma, random_state=2),
-                           data, target, 'f1', cv=2).mean()
+    val = cross_val_score(
+        SVC(C=C, gamma=gamma, random_state=2),
+        data, target, 'f1', cv=2
+    ).mean()
+
+    return val
 
 def rfccv(n_estimators, min_samples_split, max_features):
-    return cross_val_score(RFC(n_estimators=int(n_estimators),
-                               min_samples_split=int(min_samples_split),
-                               max_features=min(max_features, 0.999),
-                               random_state=2),
-                           data, target, 'f1', cv=2).mean()
+    val = cross_val_score(
+        RFC(n_estimators=int(n_estimators),
+            min_samples_split=int(min_samples_split),
+            max_features=min(max_features, 0.999),
+            random_state=2
+        ),
+        data, target, 'f1', cv=2
+    ).mean()
+    return val
 
 if __name__ == "__main__":
-    gp_params = {"alpha": 1e5}
+    gp_params = {"alpha": 1e-5}
 
-    svcBO = BayesianOptimization(svccv, {'C': (0.001, 100), 'gamma': (0.0001, 0.1)})
+    svcBO = BayesianOptimization(svccv,
+        {'C': (0.001, 100), 'gamma': (0.0001, 0.1)})
     svcBO.explore({'C': [0.001, 0.01, 0.1], 'gamma': [0.001, 0.01, 0.1]})
 
-    rfcBO = BayesianOptimization(rfccv, {'n_estimators': (10, 250),
-                                         'min_samples_split': (2, 25),
-                                         'max_features': (0.1, 0.999)})
+    rfcBO = BayesianOptimization(
+        rfccv,
+        {'n_estimators': (10, 250),
+        'min_samples_split': (2, 25),
+        'max_features': (0.1, 0.999)}
+    )
 
     svcBO.maximize(n_iter=10, **gp_params)
-    print('-'*53)
+    print('-' * 53)
     rfcBO.maximize(n_iter=10, **gp_params)
 
-    print('-'*53)
+    print('-' * 53)
     print('Final Results')
     print('SVC: %f' % svcBO.res['max']['max_val'])
     print('RFC: %f' % rfcBO.res['max']['max_val'])
