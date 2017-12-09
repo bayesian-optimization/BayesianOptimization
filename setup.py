4cf4c2d205edd0e118f4c36911ccd891a9f03688
fmfn <fmfnogueira@gmail.com>
2017-12-09 10:03:35 -0600

    Update setup file for new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Fernando Nogueira',
     author_email="fmfnogueira@gmail.com",
     description='Bayesian Optimization package',
-    download_url='https://github.com/fmfn/BayesianOptimization/tarball/0.4',
+    download_url='https://github.com/fmfn/BayesianOptimization/tarball/0.6',
     install_requires=[
         "numpy >= 1.9.0",
         "scipy >= 0.14.0",
