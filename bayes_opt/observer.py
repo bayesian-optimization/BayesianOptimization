c8f4292ede015ad271930269ae20fb71529b144f
fmfn <fernando@chatkit.com>
2018-11-07 17:17:33 -0500

    Refactor BO main object
    This commit performs a major refactor of BO main object and objects that
    support it. New methods were introduced to facilitate using this API in
    more flexible ways, nevertheless, the same out of the box behavious is
    still present keeping the package friendly.
    
    Notable changes:
    - New method register: A clean and consistent new api for the old
    init/initialize. Basicallyu provides a way for users (internal and
    external) to specify points with known target values. In the background
    this is handled by the internal target space object.
    - New method probe: A clean and consistent new api for the old explore
    method. Basically provides a way for users to force the algorithm to
    probe certain points in the parameter space. It can be done in a lazy
    (default) fashion as well as immediately.
    - Overhaul of target space object. This object was simplyfied and
    cleaned, it is now a lot easier to read and use and interfaces well with
    BO.
    
    wip - obs insp refactor of bo object
    
    slimdown target space object, easier to read and less bloat
    
    maximize method leveraging observe, inspect and internal space
    
    add events and improve prime queue method
    
    fix bugs with maximize, now working
    
    suppress gaussian process warnings
    
    clean and improve max and res availability
    
    organize events and define default events
    
    universal as_array that can handle dict and array params
    
    working refactoring of bo object

--- bayes_opt/observer.py
+++ bayes_opt/observer.py
@@ -1,24 +1,31 @@
-# Inspired/Taken from https://www.protechtraining.com/blog/post/879#simple-observer
+"""
 
 
-class Observer:
-    def update(self, event, instance):
-        # Avoid circular import
-        from .bayesian_optimization import Events
-        if event is Events.INIT_DONE:
-            print("Initialization completed")
-        elif event is Events.FIT_STEP_DONE:
-            print("Optimization step finished, current max: ", instance.res['max'])
-        elif event is Events.FIT_DONE:
-            print("Optimization finished, maximum value at: ", instance.res['max'])
+Inspired/Taken from https://www.protechtraining.com/blog/post/879#simple-observer
+"""
+
+
+class Events(object):
+    INIT_DONE = 'initialized'
+    FIT_STEP_DONE = 'fit_step_done'
+    FIT_DONE = 'fit_done'
+
+
+DEFAULT_EVENTS = [
+    Events.INIT_DONE,
+    Events.FIT_STEP_DONE,
+    Events.FIT_DONE
+]
 
 
 class Observable(object):
-    def __init__(self, events):
+    def __init__(self, events=None):
         # maps event names to subscribers
         # str -> dict
-        self.events = {event: dict()
-                       for event in events}
+        if events is None:
+            events = DEFAULT_EVENTS
+
+        self.events = {event: dict() for event in events}
 
     def get_subscribers(self, event):
         return self.events[event]
@@ -34,3 +41,17 @@ class Observable(object):
     def dispatch(self, event):
         for subscriber, callback in self.get_subscribers(event).items():
             callback(event, self)
+
+
+class Observer:
+    def update(self, event, instance):
+        # Avoid circular import
+        from .bayesian_optimization import Events
+        if event is Events.INIT_DONE:
+            print("Initialization completed")
+        elif event is Events.FIT_STEP_DONE:
+            print("Optimization step finished, current max: ",
+                  instance.res['max'])
+        elif event is Events.FIT_DONE:
+            print("Optimization finished, maximum value at: ",
+                  instance.res['max'])
--- bayes_opt/target_space.py
+++ bayes_opt/target_space.py
@@ -20,10 +20,10 @@ class TargetSpace(object):
     >>> pbounds = {'p1': (0, 1), 'p2': (1, 100)}
     >>> space = TargetSpace(target_func, pbounds, random_state=0)
     >>> x = space.random_points(1)[0]
-    >>> y = space.observe_point(x)
+    >>> y = space.register_point(x)
     >>> assert self.max_point()['max_val'] == y
     """
-    def __init__(self, target_func, pbounds, random_state=None):
+    def __init__(self, target_func, pbounds: dict, random_state=None):
         """
         Parameters
         ----------
@@ -37,110 +37,85 @@ class TargetSpace(object):
         random_state : int, RandomState, or None
             optionally specify a seed for a random number generator
         """
-
         self.random_state = ensure_rng(random_state)
 
-        # Some function to be optimized
+        # The function to be optimized
         self.target_func = target_func
 
         # Get the name of the parameters
-        self.keys = list(pbounds.keys())
+        self._keys = sorted(pbounds)
         # Create an array with parameters bounds
-        self.bounds = np.array(list(pbounds.values()), dtype=np.float)
-        # Find number of parameters
-        self.dim = len(self.keys)
+        self._bounds = np.array(list(pbounds.values()), dtype=np.float)
 
         # preallocated memory for X and Y points
-        self._Xarr = None
-        self._Yarr = None
+        self._x = np.empty(shape=(0, self.dim))
+        self._target = np.empty(shape=(0))
 
-        # Number of observations
-        self._length = 0
-
-        # Views of the preallocated arrays showing only populated data
-        self._Xview = None
-        self._Yview = None
-
-        self._cache = {}  # keep track of unique points we have seen so far
-
-    @property
-    def X(self):
-        return self._Xview
-
-    @property
-    def Y(self):
-        return self._Yview
+        # keep track of unique points we have seen so far
+        self._cache = {}
 
     def __contains__(self, x):
         return _hashable(x) in self._cache
 
     def __len__(self):
-        return self._length
+        assert len(self._x) == len(self._target)
+        return len(self._target)
 
-    def _dict_to_points(self, points_dict):
-        """
-        Example:
-        -------
-        >>> pbounds = {'p1': (0, 1), 'p2': (1, 100)}
-        >>> space = TargetSpace(lambda p1, p2: p1 + p2, pbounds)
-        >>> points_dict = {'p1': [0, .5, 1], 'p2': [0, 1, 2]}
-        >>> space._dict_to_points(points_dict)
-        [[0, 0], [1, 0.5], [2, 1]]
-        """
-        # Consistency check
-        param_tup_lens = []
+    @property
+    def empty(self):
+        return len(self) == 0
 
-        for key in self.keys:
-            param_tup_lens.append(len(list(points_dict[key])))
+    @property
+    def x(self):
+        return self._x
 
-        if all([e == param_tup_lens[0] for e in param_tup_lens]):
-            pass
-        else:
-            raise ValueError('The same number of initialization points '
-                             'must be entered for every parameter.')
+    @property
+    def target(self):
+        return self._target
 
-        # Turn into list of lists
-        all_points = []
-        for key in self.keys:
-            all_points.append(points_dict[key])
+    @property
+    def dim(self):
+        return len(self._keys)
 
-        # Take transpose of list
-        points = list(map(list, zip(*all_points)))
-        return points
+    @property
+    def keys(self):
+        return self._keys
 
-    def observe_point(self, x):
-        """
-        Evaulates a single point x, to obtain the value y and then records them
-        as observations.
+    @property
+    def bounds(self):
+        return self._bounds
 
-        Notes
-        -----
-        If x has been previously seen returns a cached value of y.
+    def params_to_array(self, params):
+        try:
+            assert set(params) == set(self.keys)
+        except AssertionError:
+            raise ValueError(
+                "Parameters' keys ({}) do ".format(sorted(params)) +
+                "not match the expected set of keys ({}).".format(self.keys)
+            )
+        return np.asarray([params[key] for key in self.keys])
 
-        Parameters
-        ----------
-        x : ndarray
-            a single point, with len(x) == self.dim
+    def array_to_params(self, x):
+        try:
+            assert len(x) == len(self.keys)
+        except AssertionError:
+            raise ValueError(
+                "Size of array ({}) is different than the ".format(len(x)) +
+                "expected number of parameters ({}).".format(len(self.keys))
+            )
+        return dict(zip(self.keys, x))
 
-        Returns
-        -------
-        y : float
-            target function value.
-        """
-        x = np.asarray(x).ravel()
-        assert x.size == self.dim, 'x must have the same dimensions'
+    def _as_array(self, x):
+        try:
+            x = np.asarray(x, dtype=float)
+        except TypeError:
+            x = self.params_to_array(x)
+        finally:
+            x = x.ravel()
+            assert x.size == self.dim, 'x must have the same dimensions'
+        return x
 
-        if x in self:
-            # Lookup previously seen point
-            y = self._cache[_hashable(x)]
-        else:
-            # measure the target function
-            params = dict(zip(self.keys, x))
-            y = self.target_func(**params)
-            self.add_observation(x, y)
-        return y
-
-    def add_observation(self, x, y):
+    def register(self, x, target):
         """
         Append a point and its target value to the known data.
 
@@ -173,94 +148,87 @@ class TargetSpace(object):
         >>> len(space)
         1
         """
+        x = self._as_array(x)
         if x in self:
             raise KeyError('Data point {} is not unique'.format(x))
 
-        if self._length >= self._n_alloc_rows:
-            self._allocate((self._length + 1) * 2)
-
-        x = np.asarray(x).ravel()
-
         # Insert data into unique dictionary
-        self._cache[_hashable(x)] = y
+        self._cache[_hashable(x.ravel())] = target
 
-        # Insert data into preallocated arrays
-        self._Xarr[self._length] = x
-        self._Yarr[self._length] = y
-        # Expand views to encompass the new data point
-        self._length += 1
+        self._x = np.concatenate([self._x, x.reshape(1, -1)])
+        self._target = np.concatenate([self._target, [target]])
 
-        # Create views of the data
-        self._Xview = self._Xarr[:self._length]
-        self._Yview = self._Yarr[:self._length]
-
-    def _allocate(self, num):
+    def probe(self, x):
         """
-        Allocate enough memory to store `num` points
-        """
-        if num <= self._n_alloc_rows:
-            raise ValueError('num must be larger than current array length')
+        Evaulates a single point x, to obtain the value y and then records them
+        as observations.
 
-        self._assert_internal_invariants()
-
-        # Allocate new memory
-        _Xnew = np.empty((num, self.bounds.shape[0]))
-        _Ynew = np.empty(num)
-
-        # Copy the old data into the new
-        if self._Xarr is not None:
-            _Xnew[:self._length] = self._Xarr[:self._length]
-            _Ynew[:self._length] = self._Yarr[:self._length]
-        self._Xarr = _Xnew
-        self._Yarr = _Ynew
-
-        # Create views of the data
-        self._Xview = self._Xarr[:self._length]
-        self._Yview = self._Yarr[:self._length]
-
-    @property
-    def _n_alloc_rows(self):
-        """ Number of allocated rows """
-        return 0 if self._Xarr is None else self._Xarr.shape[0]
-
-    def random_points(self, num):
-        """
-        Creates random points within the bounds of the space
+        Notes
+        -----
+        If x has been previously seen returns a cached value of y.
 
         Parameters
         ----------
-        num : int
-            Number of random points to create
+        x : ndarray
+            a single point, with len(x) == self.dim
+
+        Returns
+        -------
+        y : float
+            target function value.
+        """
+        x = self._as_array(x)
+
+        try:
+            y = self._cache[_hashable(x)]
+        except KeyError:
+            params = dict(zip(self._keys, x))
+            target = self.target_func(**params)
+            self.register(x, target)
+        return target
+
+    def random_sample(self):
+        """
+        Creates random points within the bounds of the space.
 
         Returns
         ----------
         data: ndarray
-            [num x dim] array points with dimensions corresponding to `self.keys`
+            [num x dim] array points with dimensions corresponding to `self._keys`
 
         Example
         -------
         >>> target_func = lambda p1, p2: p1 + p2
         >>> pbounds = {'p1': (0, 1), 'p2': (1, 100)}
         >>> space = TargetSpace(target_func, pbounds, random_state=0)
-        >>> space.random_points(3)
-        array([[ 55.33253689,   0.54488318],
-               [ 71.80374727,   0.4236548 ],
-               [ 60.67357423,   0.64589411]])
+        >>> space.random_points(1)
+        array([[ 55.33253689,   0.54488318]])
         """
         # TODO: support integer, category, and basic scipy.optimize constraints
-        data = np.empty((num, self.dim))
-        for col, (lower, upper) in enumerate(self.bounds):
-            data.T[col] = self.random_state.uniform(lower, upper, size=num)
-        return data
+        data = np.empty((1, self.dim))
+        for col, (lower, upper) in enumerate(self._bounds):
+            data.T[col] = self.random_state.uniform(lower, upper, size=1)
+        return data.ravel()
 
-    def max_point(self):
-        """
-        Return the current parameters that best maximize target function with
-        that maximum value.
-        """
-        return {'max_val': self.Y.max(),
-                'max_params': dict(zip(self.keys,
-                                       self.X[self.Y.argmax()]))}
+    def max(self):
+        """Get maximum target value found and corresponding parametes."""
+        try:
+            res = {
+                'target': self.target.max(),
+                'params': dict(zip(self.keys, self.x[self.target.argmax()]))
+            }
+        except ValueError:
+            res = {}
+        return res
+
+    def res(self):
+        """Get all target values found and corresponding parametes."""
+        params = [dict(zip(self.keys, p)) for p in self.x]
+
+        return [
+            {"target": target, "param": param}
+            for target, param in zip(self.target, params)
+        ]
 
     def set_bounds(self, new_bounds):
         """
@@ -271,30 +239,6 @@ class TargetSpace(object):
         new_bounds : dict
             A dictionary with the parameter name and its new bounds
         """
-        # Loop through the all bounds and reset the min-max bound matrix
-        for row, key in enumerate(self.keys):
+        for row, key in enumerate(self._keys):
             if key in new_bounds:
-                self.bounds[row] = new_bounds[key]
-
-    def _assert_internal_invariants(self, fast=True):
-        """
-        Run internal consistency checks to ensure that data structure
-        assumptions have not been violated.
-        """
-        if self._Xarr is None:
-            assert self._Yarr is None
-            assert self._Xview is None
-            assert self._Yview is None
-        else:
-            assert self._Yarr is not None
-            assert self._Xview is not None
-            assert self._Yview is not None
-            assert len(self._Xview) == self._length
-            assert len(self._Yview) == self._length
-            assert len(self._Xarr) == len(self._Yarr)
-
-            if not fast:
-                # run slower checks
-                assert np.all(unique_rows(self.X))
-                # assert np.may_share_memory(self._Xview, self._Xarr)
-                # assert np.may_share_memory(self._Yview, self._Yarr)
+                self._bounds[row] = new_bounds[key]
