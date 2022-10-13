eba4ff518945586765bac243e03c7c9a94885e08
till-m <musertill@gmail.com>
2022-10-13 12:44:20 +0200

    Fix a bug occuring when constraints of type `int` are used.

--- bayes_opt/constraint.py
+++ bayes_opt/constraint.py
@@ -40,15 +40,8 @@ class ConstraintModel():
     def __init__(self, fun, lb, ub, random_state=None):
         self.fun = fun
 
-        if isinstance(lb, float):
-            self._lb = np.array([lb])
-        else:
-            self._lb = lb
-        
-        if isinstance(ub, float):
-            self._ub = np.array([ub])
-        else:
-            self._ub = ub
+        self._lb = np.atleast_1d(lb)        
+        self._ub = np.atleast_1d(ub)
 
         if np.any(self._lb >= self._ub):
             msg = "Lower bounds must be less than upper bounds."
