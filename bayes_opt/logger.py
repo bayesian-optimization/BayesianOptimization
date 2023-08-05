201c6b24d4a8e93d442a46ad4a2980adfc5df1ea
till-m <36440677+till-m@users.noreply.github.com>
2023-08-05 16:10:14 +0200

    Replace custom colour implementation, add docs for `logger.py`, `util.py` (#435)
    * Replace custom colour implementation, add docs for `logger.py`, `util.py`
    
    * minor typo/syntax fixes
    
    * User `or` to separate different possible types

--- bayes_opt/logger.py
+++ bayes_opt/logger.py
@@ -1,17 +1,48 @@
+"""Contains classes and functions for logging."""
 from __future__ import print_function
 import os
 import json
-import warnings
-from pathlib import Path
 from .observer import _Tracker
 from .event import Events
-from .util import Colours
+from colorama import Fore, just_fix_windows_console
+
+just_fix_windows_console()
 
 def _get_default_logger(verbose, is_constrained):
+    """
+    Return the default logger.
+
+    Parameters
+    ----------
+    verbose : int
+        Verbosity level of the logger.
+
+    is_constrained : bool
+        Whether the underlying optimizer uses constraints (this requires
+        an additional column in the output).
+
+    Returns
+    -------
+    ScreenLogger
+        The default logger.
+
+    """
     return ScreenLogger(verbose=verbose, is_constrained=is_constrained)
 
 
 class ScreenLogger(_Tracker):
+    """Logger that outputs text, e.g. to log to a terminal.
+    
+    Parameters
+    ----------
+    verbose : int
+        Verbosity level of the logger.
+
+    is_constrained : bool
+        Whether the logger is associated with a constrained optimization
+        instance.
+    """
+
     _default_cell_size = 9
     _default_precision = 4
 
@@ -19,64 +50,106 @@ class ScreenLogger(_Tracker):
         self._verbose = verbose
         self._is_constrained = is_constrained
         self._header_length = None
-        super(ScreenLogger, self).__init__()
+        super().__init__()
 
     @property
     def verbose(self):
+        """Return the verbosity level."""
         return self._verbose
 
     @verbose.setter
     def verbose(self, v):
+        """Set the verbosity level.
+
+        Parameters
+        ----------
+        v : int
+            New verbosity level of the logger.
+        """
         self._verbose = v
 
     @property
     def is_constrained(self):
+        """Return whether the logger is constrained."""
         return self._is_constrained
 
     def _format_number(self, x):
+        """Format a number.
+
+        Parameters
+        ----------
+        x : number
+            
+
+        Returns
+        -------
+        A stringified, formatted version of `x`.
+        """
         if isinstance(x, int):
-                s = "{x:<{s}}".format(
-                    x=x,
-                    s=self._default_cell_size,
-                )
+            s = f"{x:<{self._default_cell_size}}"
         else:
-            s = "{x:<{s}.{p}}".format(
-                x=x,
-                s=self._default_cell_size,
-                p=self._default_precision,
-            )
+            s = f"{x:<{self._default_cell_size}.{self._default_precision}}"
 
         if len(s) > self._default_cell_size:
             if "." in s:
                 return s[:self._default_cell_size]
-            else:
-                return s[:self._default_cell_size - 3] + "..."
+            return s[:self._default_cell_size - 3] + "..."
         return s
 
     def _format_bool(self, x):
+        """Format a boolean.
+
+        Parameters
+        ----------
+        x : boolean
+            
+
+        Returns
+        -------
+        A stringified, formatted version of `x`.
+        """
         if 5 > self._default_cell_size:
-            if x == True:
+            if x:
                 x_ = 'T'
-            elif x == False:
+            else:
                 x_ = 'F'
         else:
             x_ = str(x)
-        s = "{x:<{s}}".format(
-            x=x_,
-            s=self._default_cell_size,
-        )
+        s = f"{x_:<{self._default_cell_size}}"
         return s
 
     def _format_key(self, key):
-        s = "{key:^{s}}".format(
-            key=key,
-            s=self._default_cell_size
-        )
+        """Format a key.
+
+        Parameters
+        ----------
+        key : string
+            
+
+        Returns
+        -------
+        A stringified, formatted version of `x`.
+        """
+        s = f"{key:^{self._default_cell_size}}"
         if len(s) > self._default_cell_size:
             return s[:self._default_cell_size - 3] + "..."
         return s
 
-    def _step(self, instance, colour=Colours.black):
+    def _step(self, instance, colour=Fore.BLACK):
+        """Log a step.
+
+        Parameters
+        ----------
+        instance : bayesian_optimization.BayesianOptimization
+            The instance associated with the event.
+            
+        colour :
+            (Default value = Fore.BLACK)
+
+        Returns
+        -------
+        A stringified, formatted version of the most recent optimization step.
+        """
         res = instance.res[-1]
         cells = []
 
@@ -89,9 +162,20 @@ class ScreenLogger(_Tracker):
         for key in instance.space.keys:
             cells.append(self._format_number(res["params"][key]))
 
-        return "| " + " | ".join(map(colour, cells)) + " |"
+        return "| " + " | ".join([colour + cells[i] for i in range(len(cells))]) + " |"
 
     def _header(self, instance):
+        """Print the header of the log.
+
+        Parameters
+        ----------
+        instance : bayesian_optimization.BayesianOptimization
+            The instance associated with the header.
+
+        Returns
+        -------
+        A stringified, formatted version of the most header.
+        """
         cells = []
         cells.append(self._format_key("iter"))
         cells.append(self._format_key("target"))
@@ -107,6 +191,17 @@ class ScreenLogger(_Tracker):
         return line + "\n" + ("-" * self._header_length)
 
     def _is_new_max(self, instance):
+        """Check if the step to log produced a new maximum.
+
+        Parameters
+        ----------
+        instance : bayesian_optimization.BayesianOptimization
+            The instance associated with the step.
+
+        Returns
+        -------
+        boolean
+        """
         if instance.max is None:
             # During constrained optimization, there might not be a maximum
             # value since the optimizer might've not encountered any points
@@ -117,6 +212,17 @@ class ScreenLogger(_Tracker):
         return instance.max["target"] > self._previous_max
 
     def update(self, event, instance):
+        """Handle incoming events.
+
+        Parameters
+        ----------
+        event : str
+            One of the values associated with `Events.OPTIMIZATION_START`,
+            `Events.OPTIMIZATION_STEP` or `Events.OPTIMIZATION_END`.
+            
+        instance : bayesian_optimization.BayesianOptimization
+            The instance associated with the step.
+        """
         if event == Events.OPTIMIZATION_START:
             line = self._header(instance) + "\n"
         elif event == Events.OPTIMIZATION_STEP:
@@ -124,7 +230,7 @@ class ScreenLogger(_Tracker):
             if self._verbose == 1 and not is_new_max:
                 line = ""
             else:
-                colour = Colours.purple if is_new_max else Colours.black
+                colour = Fore.MAGENTA if is_new_max else Fore.BLACK
                 line = self._step(instance, colour=colour) + "\n"
         elif event == Events.OPTIMIZATION_END:
             line = "=" * self._header_length + "\n"
@@ -135,18 +241,43 @@ class ScreenLogger(_Tracker):
 
 
 class JSONLogger(_Tracker):
+    """
+    Logger that outputs steps in JSON format.
+
+    The resulting file can be used to restart the optimization from an earlier state.
+    """
+
     def __init__(self, path, reset=True):
+        """
+        Initialize the logger.
+        
+        Parameters
+        ----------
+        path : str or bytes or os.PathLike
 
-
+        """
         self._path = path
         if reset:
             try:
                 os.remove(self._path)
             except OSError:
                 pass
-        super(JSONLogger, self).__init__()
+        super().__init__()
 
     def update(self, event, instance):
+        """
+        Handle incoming events.
+
+        Parameters
+        ----------
+        event : str
+            One of the values associated with `Events.OPTIMIZATION_START`,
+            `Events.OPTIMIZATION_STEP` or `Events.OPTIMIZATION_END`.
+            
+        instance : bayesian_optimization.BayesianOptimization
+            The instance associated with the step.
+
+        """
         if event == Events.OPTIMIZATION_STEP:
             data = dict(instance.res[-1])
 
