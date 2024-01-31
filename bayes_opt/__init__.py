a6e559bfe4e267e692d646c27dc0f6b3f9ebd888
unknown <bwheelz360@gmail.com>
2024-01-31 14:37:46 +1100

    switched to poetry packaging

--- bayes_opt/__init__.py
+++ bayes_opt/__init__.py
@@ -4,6 +4,12 @@ from .util import UtilityFunction
 from .logger import ScreenLogger, JSONLogger
 from .constraint import ConstraintModel
 
+import importlib.metadata
+# __version__ = importlib.metadata.version(__name__)
+# nb: this wont work unless the package name (in pyproject.toml) and the package are consistently named.
+
+
+
 __all__ = [
     "BayesianOptimization",
     "ConstraintModel",
