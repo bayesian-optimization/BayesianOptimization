41c98e5b111030822ccbf166260adba4c8b0dedb
James Krach <69264125+jameskrach@users.noreply.github.com>
2025-07-15 19:47:31 -0400

    Use just uv instead of uv and poetry

--- scripts/check_precommit.sh
+++ scripts/check_precommit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 set -ex
 
-poetry run pre-commit install
-poetry run pre-commit run --all-files --show-diff-on-failure
+uv run pre-commit install
+uv run pre-commit run --all-files --show-diff-on-failure
